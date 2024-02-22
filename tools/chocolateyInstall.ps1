da5d993c59556dd9466f64ca4176c94f375fa9f9
Alexander Bejarano <abhieud@yahoo.es>
2024-02-22 17:37:29 +0100

    Update chocolateyInstall.ps1
    version 11.2.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,8 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'midori-browser'
+#﻿$packageName = '{{PackageName}}'
 $installerType = 'EXE'
-$url = '{{DownloadUrl}}'
+$url = 'https://github.com/goastian/midori-desktop/releases/download/v11.2.2/midori-11.2.2.win64.installer.exe'
+#$url = '{{DownloadUrl}}'
 $silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
